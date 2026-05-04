e4f4957076179285b2d732c0894f5996d5649501
Rohrr Agustín <85656346+agustinrohrr11@users.noreply.github.com>
2026-05-03 22:25:02 -0300

    pruebas y cambios en main

--- LCCA/main.py
+++ LCCA/main.py
@@ -2,8 +2,12 @@ from modules.area_manager import AreaManager
 from modules.satellite_provider import SatelliteProvider
 from modules.raster_analyzer import RasterAnalyzer # <--- Nuevo
 import imageio
+import numpy as np
 
-def run():
+
+def generar_imagen_analisis():
+    ##### imagen satelital de un área específica #####
+
     # 1. Ubicación
     mi_zona = AreaManager(lat=-31.82, lon=-60.51, radio_km=1)
     
@@ -26,5 +30,29 @@ def run():
     imageio.imwrite('resultado_analisis.png', imagen_final)
     print("Imagen 'resultado_analisis.png' generada con éxito.")
 
+
+
+def imprimir_resumen_pixeles():
+
+    # 1. Ubicación
+    mi_zona = AreaManager(lat=-31.82, lon=-60.51, radio_km=1)
+    
+    # 2. Obtención de datos
+    prov = SatelliteProvider()
+    item = prov.buscar_imagen_reciente(mi_zona.obtener_bbox_latlon())
+    matriz_cruda = prov.descargar_ventana(item, mi_zona, banda="B04")
+    
+    # 3. ANÁLISIS (Aquí empieza la magia)
+    analyzer = RasterAnalyzer(matriz_cruda)
+        
+    #####    Para no saturar la consola, imprimimos solo un resumen de los valores    #####
+    #        Tomamos una muestra de los valores para no saturar la consola
+    print(f"Valor mínimo de reflexión: {np.min(analyzer.data)}")
+    print(f"Valor máximo de reflexión: {np.max(analyzer.data)}")
+    print(f"Promedio de brillo en Oro Verde: {np.mean(analyzer.data):.2f}")
+
+
 if __name__ == "__main__":
-    run()
+
+   generar_imagen_analisis()
+   imprimir_resumen_pixeles()
